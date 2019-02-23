b25148103c15e195085057530dc6078856306ce8
Andrew Nel <andrew.nel.an@gmail.com>
2019-02-23 17:18:58 +0200

    Added test for the Order's Price method using mocks for flower objects

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -20,12 +20,26 @@ namespace Tests
             IOrderDAO y = Substitute.For<IOrderDAO>();
             List<Flower> f = new List<Flower>();
             IOrder o = new Order(y,c,false);
-
-
             //ACT==============================
             o.Deliver();
             //ASSERT===========================
             y.Received().SetDelivered(Arg.Any<IOrder>());
+        }
+
+        [Test]
+        public void Test2()
+        {
+            //ARRANGE==========================
+            IClient c = Substitute.For<IClient>();
+            IOrderDAO y = Substitute.For<IOrderDAO>();
+            IFlower f = Substitute.For<IFlower>(); 
+            IOrder o = new Order(y,c,false);
+            //ACT==============================
+            o.AddFlowers(f, 3);
+            double orderPrice = o.Price;
+            //ASSERT===========================
+            // Adding 3 flowers with 20% markup each (The way Price should work)
+            Assert.AreEqual(orderPrice, 3*1.2*f.Cost); 
         }
     }
 }
